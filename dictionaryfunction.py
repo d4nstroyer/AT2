358c55022f91aedf4a1efc613c0cfafecac8f75f
Jemie Effendy <jemie_effendy@yahoo.co.id>
2024-10-30 19:40:15 +0800

    fix: typo name

--- dictionaryfunction.py
+++ dictionaryfunction.py
@@ -31,7 +31,7 @@ actor_dict = {
     "Elizabeth Olsen": "February 16, 1989", 
     "Benedict Cumberbatch": "July 19, 1976", 
     "Brie Larson": "October 1, 1989", 
-    "Jamie Lannister": "October 20, 1990"
+    "Jamie Lonnister": "October 20, 1990"
 }
 
 movie_dict = {
